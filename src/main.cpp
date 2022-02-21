0946110ea637caafd7a27b7b536e552225f8e6f7
noahwhygodwhy <noah.mathnerd.alvard@gmail.com>
2022-02-21 15:01:46 -0500

    added GLFW source so we can just build it each time

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+
+const int screenX = 1000;
+const int screenY = 1000;
+
+
+int main() {
+    printf("hello world?\n");
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SRGB_CAPABLE, 1);
+    glfwWindowHint(GLFW_SAMPLES, 16);
+    GLFWwindow* window = glfwCreateWindow(screenX, screenY, "Title Goes here", NULL, NULL);
+
+    if (window == NULL)
+    {
+        cout << "Window creation failed" << endl;
+        exit(-1);
+    }
+
+    glfwMakeContextCurrent(window);
+    printf("window creation succeeded?????\n");
+}
